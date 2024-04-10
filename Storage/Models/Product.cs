c61b965b49e79ef252a70f4a85303b36902296c6
Mattias Hammarhorn <mattias@hammarhorn.se>
2024-04-10 17:43:42 +0200

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+        
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+        
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Orderdate { get; set; }
+        public string Category { get; set; }
+        
+        [Required]
+        public string Shelf { get; set; }
+        
+        [Range(1,9999)]
+        public int Count { get; set; }
+        public string Description { get; set; }
+    }
+}
